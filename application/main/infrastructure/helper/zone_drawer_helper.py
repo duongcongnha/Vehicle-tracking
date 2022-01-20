ef21de08cccfc2c9b25264e9ca2f219b8c07cf3a
Nha Duong <duongcongnhahs@gmail.com>
2022-01-20 10:12:44 +0700

    update params

--- application/main/infrastructure/helper/zone_drawer_helper.py
+++ application/main/infrastructure/helper/zone_drawer_helper.py
@@ -6,7 +6,7 @@ class ZoneDrawerHelper():
     def __init__(self) -> None:
         self.custom_green = (2, 115, 36)
         self.font = cv2.FONT_HERSHEY_DUPLEX
-    def draw(self, im0:np.ndarray, frame_width,frame_height:int, upper_ratio=1/2.65, lower_ratio=7/8):
+    def draw(self, im0:np.ndarray, frame_width,frame_height:int, upper_ratio, lower_ratio):
         
         # top zone
         x, y, x1, y1 = 0,0, frame_width, int(frame_height*upper_ratio)
@@ -26,8 +26,6 @@ class ZoneDrawerHelper():
         overlay = copy.deepcopy(zone)
 
         cv2.rectangle(overlay, (0, 0), (overlay.shape[1], overlay.shape[0]), (0, 0, 200), -1)
-        # cv2.putText(overlay, "UNUSED ZONE", (x+25, y+35), self.font, 1, (255, 255, 255), 2, cv2.LINE_AA)
         
-        alpha = 0.5
         im0[y:y1, x:x1] = cv2.addWeighted(overlay, alpha, zone, 1 - alpha, 0)
 
