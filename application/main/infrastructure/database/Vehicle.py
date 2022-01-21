030d144ddc8ee6e370ebcbf811016dc98955d1fd
Nha Duong <duongcongnhahs@gmail.com>
2022-01-21 16:39:18 +0700

    add LANE to vehicle info

--- application/main/infrastructure/database/Vehicle.py
+++ application/main/infrastructure/database/Vehicle.py
@@ -10,12 +10,14 @@ class Vehicle(Base):
     in_time = Column(DateTime)
     exit_time = Column(DateTime)
     type_vehicle = Column(String)
+    lane = Column(String)
     
-    def __init__(self, ID, in_time, exit_time, type_vehicle):
+    def __init__(self, ID, in_time, exit_time, type_vehicle, lane):
         self.ID = ID
         self.in_time = in_time
         self.exit_time = exit_time
         self.type_vehicle = type_vehicle
+        self.lane = lane
 
     def to_dict(self):
         FMT = "%d-%m-%Y %H:%M:%S"
@@ -24,4 +26,5 @@ class Vehicle(Base):
         vehicle_dict[self.ID]['in_time'] = self.in_time.strftime(FMT)
         vehicle_dict[self.ID]['exit_time'] = self.exit_time.strftime(FMT)
         vehicle_dict[self.ID]['type_vehicle'] = self.type_vehicle
+        vehicle_dict[self.ID]['lane'] = self.lane
         return vehicle_dict
